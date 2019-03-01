8274c0a0844c4d909389e40a7c86abbc52d79c0a
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-03-01 09:35:26 +0200

    Add validation func

--- fn.go
+++ fn.go
@@ -2,30 +2,35 @@ package function
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/eldimious/slack-golang-gcf/config"
 	dispatcher "github.com/eldimious/slack-golang-gcf/data/dispatcher"
 	messages "github.com/eldimious/slack-golang-gcf/domain/messages"
+	validator "github.com/eldimious/slack-golang-gcf/router"
 )
 
-type messageValidator struct {
-	Text      string
-	Username  string
-	Channel   string
-	IconEmoji string
-	Type      string
-}
-
-// SendNotification - Send notification to slack group
+// SendNotification - Send notification to slack
 func SendNotification(w http.ResponseWriter, r *http.Request) {
-	var data messageValidator
+	data := &validator.MessageValidator{}
+	defer r.Body.Close()
 	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
+		w.Header().Set("Content-type", "applciation/json")
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
+		return
+	}
+	validationErrs := validator.Validate(data)
+	if len(validationErrs) > 0 {
+		err := map[string]interface{}{"validationError": validationErrs}
+		w.Header().Set("Content-type", "applciation/json")
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Error: %s", err)
+		json.NewEncoder(w).Encode(err)
 		return
 	}
 	message := &messages.Message{
@@ -38,22 +43,26 @@ func SendNotification(w http.ResponseWriter, r *http.Request) {
 
 	configuration, err := config.NewConfig()
 	if err != nil {
+		log.Println(err.Error())
+		w.Header().Set("Content-type", "applciation/json")
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
-		log.Println(err.Error())
 		return
 	}
+
 	messagesDispatcher := dispatcher.New(configuration.Slack)
 	messagesSvc := messages.NewService(messagesDispatcher)
 	dispatcherError := messagesSvc.SendMessage(message)
 	if dispatcherError != nil {
+		w.Header().Set("Content-type", "applciation/json")
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(dispatcherError.Error()))
 		log.Println(dispatcherError.Error())
 		return
 	}
-	// all good. write our message.
+
 	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-type", "applciation/json")
 	w.Write([]byte("Message was sent!"))
 	return
 }
